526a967580e668ab2ee4da8facb0ae72ce14d558
Xin Zhang <xinzweb@users.noreply.github.com>
2017-11-27 21:35:36 -0800

    No `s-o-b` when use `git-together commit`
    By default, the `git-together commit` will add `Signed-off-by:` to the
    committer. This will be redundant after `git author` setup the commit
    template. Hence, we should disable this behavior.
    
    In this case, the `git together commit` can still alternate the author
    and committer, but it will use the template populated by `git author` to
    capture all the authors.

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@
 touch ~/.git-author
 
 git config --global commit.template ~/.git-author
+
+cat >> ~/.bashrc <<EOL
+
+# disable --signoff called by 'git-together commit'
+export GIT_TOGETHER_NO_SIGNOFF=1
+EOL
