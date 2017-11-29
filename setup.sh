e5f8f79a254f7389e1e4265d84b52474ab2ba652
C.J. Jameson <cjameson@pivotal.io>
2017-11-29 09:45:05 -0800

    Set up the ENV variable in the profile
    If someone opens up a subshell (non-login), we expect that it will
    inherit env variables as appropriate. So bash_profile would be better
    
    Author: C.J. Jameson <cjameson@pivotal.io>

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ touch ~/.git-author
 
 git config --global commit.template ~/.git-author
 
-cat >> ~/.bashrc <<EOL
+cat >> ~/.bash_profile <<EOL
 
 # disable --signoff called by 'git-together commit'
 export GIT_TOGETHER_NO_SIGNOFF=1
